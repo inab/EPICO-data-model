6c8e4f3e5620f8c67f67f742c9cdee9cbaa863e0
José María Fernández <jmfernandez@cnio.es>
2014-01-16 22:00:13 +0100

    [bugfix] Rewritten script, so it does not break on colorful gits

--- addNewModelReleases.sh
+++ addNewModelReleases.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-git status releases | grep -F '#       releases/B' | cut -c 9-|xargs git add
+git status --porcelain releases | grep -F '?? releases/B' | cut -c 4-|xargs git add
+git status --porcelain releases | grep -F '?? releases/0' | cut -c 4-|xargs git add
